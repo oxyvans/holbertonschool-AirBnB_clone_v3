735279ad3757744d68052c822db637e7459944ff
oxyvans <benja.1999.g@hotmail.com>
2022-09-08 14:18:14 +0000

    add get method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,6 +71,41 @@ class FileStorage:
 
     def get(self, cls, id):
         """ fun get """
+	if (id == None) or (cls == None) or (cls not in classes.values())
+		return None
+	k = "{}.{}".format(cls.__name__, id)
+	return (self.__objects[k])
 
     def count(self, cls=None):
         """ funt count """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
