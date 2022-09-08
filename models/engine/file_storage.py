168395cb053eee44dbf15d2778f267012a6d8ff5
oxyvans <benja.1999.g@hotmail.com>
2022-09-08 15:57:30 +0000

    add task 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,41 +71,13 @@ class FileStorage:
 
     def get(self, cls, id):
         """ fun get """
-	if (id == None) or (cls == None) or (cls not in classes.values())
-		return None
-	k = "{}.{}".format(cls.__name__, id)
-	return (self.__objects[k])
+        if (id == None) or (cls == None) or (cls not in classes.values()):
+            return None
+        k = "{}.{}".format(cls.__name__, id)
+        return (self.__objects[k])
 
     def count(self, cls=None):
         """ funt count """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (cls not in classes.values()):
+            return 0
+        return len(self.all(cls))
