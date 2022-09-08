f7cc6627e3b21fbd43d937900b531510466ac22e
oxyvans <benja.1999.g@hotmail.com>
2022-09-08 13:30:54 +0000

    add get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,9 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ fun get """
+
+    def count(self, cls=None):
+        """ funt count """
