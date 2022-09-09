1f1311cc29ae1f77c0ec8879f255b0be21996129
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-09 09:43:38 -0700

    Add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """initialize index"""
 from api.v1.views import app_views
+from models import storage
 import json
 
 
 @app_views.route('/status')
 def show():
     return ({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
+               "reviews": "Review", "states": "State", "users": "User"}
+    classes_count = {}
+    for k, v in classes.items():
+        classes_count.update({k: storage.count(v)})
+    return classes_count
